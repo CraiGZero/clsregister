50fe90bbefaae1d1b19bf663e7b4414784687118
CraiG <zhuchunguang@58pic.com>
2020-11-23 16:47:56 +0800

    fix：sass和less变量转化为字符串

--- src/fastRegister/less.js
+++ src/fastRegister/less.js
@@ -2,7 +2,7 @@ const {baseBuilder} = require('../utils')
 
 module.exports = baseBuilder(
   (key, value) => (
-    `@${key}: '${value}';`
+    `@${key}: ~'${value}';`
   ),
   (data) => (
     data.join('\n')
